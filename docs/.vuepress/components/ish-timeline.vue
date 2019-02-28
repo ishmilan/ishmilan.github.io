896e439ebc1ffb8509e9f237b1f8f53cbb94b255
ishmilan <ish.milan.dev@gmail.com>
2019-02-28 22:33:38 +0100

    deploy

---
+++ docs/.vuepress/components/ish-timeline.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="timeline">
+    <div class="timeline-legend">
+      <div class="timeline-work">Experiencia laboral</div>
+      <div class="timeline-education">Formación</div>
+      <div class="timeline-aditional">Formación adicional</div>
+      <div class="timeline-goals">Logros</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
